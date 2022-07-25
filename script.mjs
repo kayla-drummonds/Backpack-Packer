8e5270e0c264998eb1a8851f52cd613e543596c2
Michaela Drummonds <mickeysymone@gmail.com>
2022-07-25 11:03:28 -0400

    added switch statement

--- script.mjs
+++ script.mjs
@@ -59,3 +59,31 @@ navList.innerHTML = navContent;
 mainNav.append(navList);
 
 document.querySelector(".siteheader").append(mainNav);
+
+const usedStatus = () => {
+    let age = everydayPack.backpackAge();
+    let description;
+
+    switch (true) {
+        case age < 30:
+            description = "new"
+            break;
+        case age >= 30 && age < 365:
+            description = "lightly used"
+            break;
+        case age >= 365 && age < 1095:
+            description = "used"
+            break;
+        case age >= 1095:
+            description = "old"
+            break;
+        default:
+            console.log('There is no description for ${age}.');
+    }
+
+    console.log(`
+    Age: ${age} days
+    Status: ${description}
+    `);
+};
+usedStatus();
